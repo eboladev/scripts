d8b6c6819a73cb1e4106e6c68a2e90ca94ec3fcd
Akos Polster <akos.polster@greenwavereality.com>
2014-07-03 12:39:00 +0200

    .

--- posix/greenwave/qcp.sh
+++ posix/greenwave/qcp.sh
@@ -8,13 +8,14 @@ if [ -z "$LEGO_PRODUCT" ]; then
     exit 1
 fi
 
-PROJNAME=$LEGO_OUTPUT/$LEGO_PRODUCT
-find $LEGO_ROOT/proprietary $LEGO_ROOT/opensource $LEGO_ROOT/thirdparty \
-    $LEGO_ROOT/products/$LEGO_PRODUCT \
+PROJNAME=$LEGO_ROOT/$LEGO_PRODUCT
+cd $LEGO_ROOT
+find proprietary opensource thirdparty \
+    products/$LEGO_PRODUCT \
     $LEGO_OUTPUT/include -type f -iname '*.cpp' -or -iname '*.h' -or \
     -iname 'Makefile' -or -iname '*.mk' -or -iname '*.sh'-or \
     -iname '*.cfg' -or -iname '.config' -or -iname '*.menu'> $PROJNAME.files
-find $LEGO_ROOT/proprietary $LEGO_ROOT/opensource $LEGO_ROOT/thirdparty \
+find proprietary opensource thirdparty \
     $LEGO_OUTPUT/include -type d \
     > $PROJNAME.includes
 echo "[General]" > $PROJNAME.creator
